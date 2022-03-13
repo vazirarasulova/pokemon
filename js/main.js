a6ee8c910a95f97ba884c71aca2a1cf9a890ab2a
vazirarasulova <rasulovavazira202gmail.com>
2022-03-13 20:19:49 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var elList = document.querySelector(".pokemon__list");
+
+for (var pokemon of pokemons){
+
+  var newItem = document.createElement("li");
+  var newImg = document.createElement("img");
+  var newSubbox = document.createElement("div");
+  var newName = document.createElement("h2");
+  var newText = document.createElement("p");
+
+
+  newImg.textContent = pokemon.img;
+  newName.textContent = pokemon.name;
+  newText.textContent = pokemon.weaknesses.join(", ");
+
+
+
+  
+  newItem.setAttribute("class", "card mb-3 bg-info bg-opacity-25");
+  newItem.setAttribute("style", "width: 18rem");
+  newImg.setAttribute("src", pokemon.img);
+  newImg.setAttribute("class", "card-img-top");
+  newSubbox.setAttribute("class", "card-body text-center");
+  newName.setAttribute("class", "card-title");
+  newText.setAttribute("class", "card-text");
+ 
+
+
+
+  
+  newSubbox.appendChild(newName);
+  newSubbox.appendChild(newText);
+ newItem.appendChild(newImg);
+ newItem.appendChild(newSubbox);
+  elList.appendChild(newItem);
+ 
+  
+ 
+}
