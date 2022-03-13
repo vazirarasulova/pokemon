ab7508cdf5839e9ac07c3cd07c6b90671f5c9e30
vazirarasulova <rasulovavazira202gmail.com>
2022-03-13 21:33:16 +0500

    Add site Header

--- js/main.js
+++ js/main.js
@@ -19,8 +19,10 @@ for (var pokemon of pokemons){
   newItem.setAttribute("class", "card mb-3 bg-info bg-opacity-25");
   newItem.setAttribute("style", "width: 18rem");
   newImg.setAttribute("src", pokemon.img);
+  newImg.setAttribute("height", 286);
+  newImg.setAttribute("class", "card-img");
   newImg.setAttribute("class", "card-img-top");
-  newSubbox.setAttribute("class", "card-body text-center");
+  newSubbox.setAttribute("class", "card-body text-center bg-light");
   newName.setAttribute("class", "card-title");
   newText.setAttribute("class", "card-text");
  
