7d63d6563f50d557f861afd4de48cb2e65073983
vazirarasulova <rasulovavazira202gmail.com>
2022-03-14 01:19:08 +0500

    Add filter

--- js/main.js
+++ js/main.js
@@ -21,44 +21,67 @@
 // search.addEventListener("")
 
 
+
+
+
+
+
 var elList = document.querySelector(".pokemon__list");
 
-for (var pokemon of pokemons){
+var elSelect = document.querySelector(".js-select");
+var pokemons = pokemons
 
-  var newItem = document.createElement("li");
-  var newImg = document.createElement("img");
-  var newSubbox = document.createElement("div");
-  var newName = document.createElement("h2");
-  var newText = document.createElement("p");
-
-
-  newImg.textContent = pokemon.img;
-  newName.textContent = pokemon.name;
-  newText.textContent = pokemon.weaknesses.join(", ");
-
-
-
-  
-  newItem.setAttribute("class", "card mb-3 bg-info bg-opacity-25");
-  newItem.setAttribute("style", "width: 18rem");
-  newImg.setAttribute("src", pokemon.img);
-  newImg.setAttribute("height", 286);
-  newImg.setAttribute("class", "card-img");
-  newImg.setAttribute("class", "card-img-top");
-  newSubbox.setAttribute("class", "card-body text-center bg-light");
-  newName.setAttribute("class", "card-title");
-  newText.setAttribute("class", "card-text");
- 
-
-
-
-  
-  newSubbox.appendChild(newName);
-  newSubbox.appendChild(newText);
- newItem.appendChild(newImg);
- newItem.appendChild(newSubbox);
-  elList.appendChild(newItem);
- 
-  
- 
+function filterArray(array,selValue){
+  return array.filter(item => item.weaknesses.includes(selValue))
 }
+
+elSelect.addEventListener("change", function(){
+
+  func(filterArray(pokemons,elSelect.value))
+})
+
+
+function func(arr){
+  elList.innerHTML = ""
+  for (var pokemon of arr){
+
+    var newItem = document.createElement("li");
+    var newImg = document.createElement("img");
+    var newSubbox = document.createElement("div");
+    var newName = document.createElement("h2");
+    var newText = document.createElement("p");
+  
+  
+    newImg.textContent = pokemon.img;
+    newName.textContent = pokemon.name;
+    newText.textContent = pokemon.weaknesses.join(", ");
+  
+  
+  
+    
+    newItem.setAttribute("class", "card mb-3 bg-info bg-opacity-25");
+    newItem.setAttribute("style", "width: 18rem");
+    newImg.setAttribute("src", pokemon.img);
+    newImg.setAttribute("height", 286);
+    newImg.setAttribute("class", "card-img");
+    newImg.setAttribute("class", "card-img-top");
+    newSubbox.setAttribute("class", "card-body text-center bg-light");
+    newName.setAttribute("class", "card-title");
+    newText.setAttribute("class", "card-text");
+   
+  
+  
+  
+    
+    newSubbox.appendChild(newName);
+    newSubbox.appendChild(newText);
+   newItem.appendChild(newImg);
+   newItem.appendChild(newSubbox);
+    elList.appendChild(newItem);
+   
+    
+   
+  }
+}
+
+func(pokemons)
